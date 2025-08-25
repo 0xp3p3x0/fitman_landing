f30615d7f7ce7ef6965619dcd8e8e53b2345cd7b
0xp3p3x0 <luis@thirdwaveintl.net>
2025-08-25 10:44:11 -0700

    update1

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,6 @@ export default function Home() {
       <HeroSection />
       <AboutSection />
       <RoadmapSection />
-
       <TokenomicsSection />
       <UtilitiesSection />
       <UtilitiesSection2 />
